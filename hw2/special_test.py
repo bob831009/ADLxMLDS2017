bcb0d26dac9ecc6dcb4b92cce08f9e1827952612
Bob Liang <bob831009@gmail.com>
2017-11-19 21:55:30 +0800

    modify special_test make it fit to new model structure

--- hw2/special_test.py
+++ hw2/special_test.py
@@ -5,4 +5,4 @@ data_dir = sys.argv[1];
 output_file = sys.argv[2];
 model_path='./models/model-90';
 peer_output_file = 'tmp_peer_result.txt'
-model_seq2seq.test(data_dir, output_file, peer_output_file, model_path);
+model_seq2seq.test(data_dir, output_file, peer_output_file, model_path, special_test=True);
