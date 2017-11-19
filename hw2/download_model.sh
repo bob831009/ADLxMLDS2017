48674d28f351b24f73014264dd647dcc102e6075
Bob Liang <bob831009@gmail.com>
2017-11-19 21:47:05 +0800

    submit for model download

--- hw2/download_model.sh
+++ hw2/download_model.sh
@@ -1,1 +1,1 @@
-wget -O ./models/model-990.data-00000-of-00001 "https://www.dropbox.com/s/qzo479wlg1qk7kz/model-990.data-00000-of-00001?dl=1"
+wget -O ./models/model-90.data-00000-of-00001 "https://www.dropbox.com/s/jeqsrmliitnk760/model-90.data-00000-of-00001?dl=1"
--- hw2/special_test.py
+++ hw2/special_test.py
@@ -1,7 +1,8 @@
-import S2VT_model
+import model_seq2seq
 import sys
 
 data_dir = sys.argv[1];
 output_file = sys.argv[2];
-model_path='./models/model-990';
-S2VT_model.test(data_dir, output_file, model_path, special_test=True);
+model_path='./models/model-90';
+peer_output_file = 'tmp_peer_result.txt'
+model_seq2seq.test(data_dir, output_file, peer_output_file, model_path);
